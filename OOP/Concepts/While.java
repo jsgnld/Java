721afc18b52614669bb4a718b193c6eac06d3d57
Jose Mari Aguinaldo <141667707+jsgnld@users.noreply.github.com>
2024-12-28 16:57:21 +0800

    Delete OOP/Concepts directory

--- OOP/Concepts/While.java
+++
@@ -1,9 +0,0 @@
-// While Loop
-
-public class While {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
-}
